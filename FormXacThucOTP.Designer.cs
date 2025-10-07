2d02f4d599e61ecca027f99e4b609002d82cb776
Lục Vĩnh Kiệt <24520903@gm.uit.edu.vn>
2025-10-07 20:48:21 +0700

    Update FormXacThucOTP.Designer.cs

--- FormXacThucOTP.Designer.cs
+++ FormXacThucOTP.Designer.cs
@@ -46,10 +46,11 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(147, 28);
+            label1.Location = new Point(184, 35);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
             label1.RightToLeft = RightToLeft.Yes;
-            label1.Size = new Size(206, 31);
+            label1.Size = new Size(254, 38);
             label1.TabIndex = 1;
             label1.Text = "Xác thực mã OTP";
             // 
@@ -57,9 +58,10 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(23, 72);
+            label2.Location = new Point(29, 90);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(479, 20);
+            label2.Size = new Size(589, 25);
             label2.TabIndex = 5;
             label2.Text = "Vui lòng nhập mã OTP vừa gửi tới email hoặc số điện thoại của bạn ";
             label2.TextAlign = ContentAlignment.TopCenter;
@@ -67,65 +69,66 @@
             // tb_s1
             // 
             tb_s1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            tb_s1.Location = new Point(57, 112);
+            tb_s1.Location = new Point(71, 140);
             tb_s1.Margin = new Padding(2);
             tb_s1.Name = "tb_s1";
-            tb_s1.Size = new Size(45, 43);
+            tb_s1.Size = new Size(55, 50);
             tb_s1.TabIndex = 6;
             // 
             // tb_s6
             // 
             tb_s6.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            tb_s6.Location = new Point(416, 112);
+            tb_s6.Location = new Point(520, 140);
             tb_s6.Margin = new Padding(2);
             tb_s6.Name = "tb_s6";
-            tb_s6.Size = new Size(45, 43);
-            tb_s6.TabIndex = 7;
+            tb_s6.Size = new Size(55, 50);
+            tb_s6.TabIndex = 11;
             // 
             // tb_s3
             // 
             tb_s3.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            tb_s3.Location = new Point(192, 112);
+            tb_s3.Location = new Point(251, 140);
             tb_s3.Margin = new Padding(2);
             tb_s3.Name = "tb_s3";
-            tb_s3.Size = new Size(45, 43);
+            tb_s3.Size = new Size(55, 50);
             tb_s3.TabIndex = 8;
             // 
             // tb_s2
             // 
             tb_s2.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            tb_s2.Location = new Point(128, 112);
+            tb_s2.Location = new Point(160, 140);
             tb_s2.Margin = new Padding(2);
             tb_s2.Name = "tb_s2";
-            tb_s2.Size = new Size(45, 43);
-            tb_s2.TabIndex = 9;
+            tb_s2.Size = new Size(55, 50);
+            tb_s2.TabIndex = 7;
             // 
             // tb_s5
             // 
             tb_s5.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            tb_s5.Location = new Point(341, 112);
+            tb_s5.Location = new Point(426, 140);
             tb_s5.Margin = new Padding(2);
             tb_s5.Name = "tb_s5";
-            tb_s5.Size = new Size(45, 43);
+            tb_s5.Size = new Size(55, 50);
             tb_s5.TabIndex = 10;
             // 
             // tb_s4
             // 
             tb_s4.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            tb_s4.Location = new Point(271, 112);
+            tb_s4.Location = new Point(339, 140);
             tb_s4.Margin = new Padding(2);
             tb_s4.Name = "tb_s4";
-            tb_s4.Size = new Size(45, 43);
-            tb_s4.TabIndex = 11;
+            tb_s4.Size = new Size(55, 50);
+            tb_s4.TabIndex = 9;
             // 
             // btn_tieptuc
             // 
             btn_tieptuc.BackColor = Color.DarkCyan;
             btn_tieptuc.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_tieptuc.ForeColor = SystemColors.ButtonHighlight;
-            btn_tieptuc.Location = new Point(136, 192);
+            btn_tieptuc.Location = new Point(170, 240);
+            btn_tieptuc.Margin = new Padding(4, 4, 4, 4);
             btn_tieptuc.Name = "btn_tieptuc";
-            btn_tieptuc.Size = new Size(223, 38);
+            btn_tieptuc.Size = new Size(279, 48);
             btn_tieptuc.TabIndex = 12;
             btn_tieptuc.Text = "TIẾP TỤC";
             btn_tieptuc.UseVisualStyleBackColor = false;
@@ -136,9 +139,10 @@
             btn_guilaima.BackColor = Color.DarkCyan;
             btn_guilaima.Font = new Font("Segoe UI Black", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_guilaima.ForeColor = SystemColors.ButtonHighlight;
-            btn_guilaima.Location = new Point(136, 244);
+            btn_guilaima.Location = new Point(170, 305);
+            btn_guilaima.Margin = new Padding(4, 4, 4, 4);
             btn_guilaima.Name = "btn_guilaima";
-            btn_guilaima.Size = new Size(223, 38);
+            btn_guilaima.Size = new Size(279, 48);
             btn_guilaima.TabIndex = 13;
             btn_guilaima.Text = "GỬI LẠI MÃ";
             btn_guilaima.UseVisualStyleBackColor = false;
@@ -147,19 +151,20 @@
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.Logo_UIT_updated_svg;
-            pictureBox4.Location = new Point(12, 12);
+            pictureBox4.Location = new Point(15, 15);
+            pictureBox4.Margin = new Padding(4, 4, 4, 4);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(45, 38);
+            pictureBox4.Size = new Size(56, 48);
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.TabIndex = 18;
             pictureBox4.TabStop = false;
             // 
             // FormXacThucOTP
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightCyan;
-            ClientSize = new Size(512, 381);
+            ClientSize = new Size(640, 476);
             Controls.Add(pictureBox4);
             Controls.Add(btn_guilaima);
             Controls.Add(btn_tieptuc);
